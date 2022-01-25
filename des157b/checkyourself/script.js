a039ffc8622f392cc99013c4f0c3fcaf88c95d6d
Darren Wong <darwong@ucdavis.edu>
2022-01-25 00:36:32 -0800

    added other app icons and styling changes

--- des157b/checkyourself/script.js
+++ des157b/checkyourself/script.js
@@ -38,35 +38,66 @@
     };
 
 
-    let instagramIcons = [];
     function showIcons (sectionNum, globalData){
         if (sectionNum !== prevLoc){
-            const sectionIcons = document.querySelectorAll('.fa-instagram-square');
+            const sectionIcons = document.querySelectorAll('i');
             for (let i=0; i < sectionIcons.length; i++){
                 sectionIcons[i].remove();
             };
             prevLoc = sectionNum;
         };
 
-        instagramIcons = [];
         // Adds one Instagram icon every time through the for loop up until the number specified in the data
-        console.log(globalData[sectionNum].instagram);
         for(let i=0; i < globalData[sectionNum].instagram; i++) {
             const randomNum1 = Math.floor(Math.random()*1300);
             const randomNum2 = Math.floor(Math.random()*700);
-            document.getElementById('icons').innerHTML += `<i id="instagram${i}_${sectionNum}" class="fab fa-instagram-square" style="left:${randomNum1}px; top:${randomNum2}px;"></i>`;
-            instagramIcons.push(document.getElementById(`instagram${i}_${sectionNum}`));
-        };
-        console.log(instagramIcons);
 
-        // for(let i=0; i<instagramIcons.length; i++) {
-        //     console.log(instagramIcons[i]);
-        //     instagramIcons[i].style.left = `${randomNum1}px`;
-        //     instagramIcons[i].style.top = `${randomNum2}px`;
-        // };   
-        
+            document.getElementById('icons').innerHTML += `<i id="instagram${i}_${sectionNum}" class="fab fa-instagram-square" style="left:${randomNum1}px; top:${randomNum2}px;"></i>`;
+        };
+
+        for(let i=0; i < globalData[sectionNum].youtube; i++) {
+            const randomNum1 = Math.floor(Math.random()*1300);
+            const randomNum2 = Math.floor(Math.random()*700);
+
+            document.getElementById('icons').innerHTML += `<i id="youtube${i}_${sectionNum}" class="fab fa-youtube" style="left:${randomNum1}px; top:${randomNum2}px;"></i>`;
+        };
+
+        for(let i=0; i < globalData[sectionNum].messages; i++) {
+            const randomNum1 = Math.floor(Math.random()*1300);
+            const randomNum2 = Math.floor(Math.random()*700);
+
+            document.getElementById('icons').innerHTML += `<i id="messages${i}_${sectionNum}" class="fas fa-comment" style="left:${randomNum1}px; top:${randomNum2}px;"></i>`;
+        };
     };
 
-    // showIcons(sectionNum, globalData);
+    // I tried to condense the code but couldn't get access the data values with the apps array
+
+    // function showIcons (sectionNum, globalData){
+    //     const apps = ["youtube", "instagram", "messages"];
+    //     console.log(globalData[sectionNum].apps[0]);
+
+    //     for (let i=0; i<apps.length; i++){
+            
+    //         if (sectionNum !== prevLoc){
+    //             const sectionIcons = document.querySelectorAll('i');
+    //             for (let i=0; i < sectionIcons.length; i++){
+    //                 sectionIcons[i].remove();
+    //             };
+    //             prevLoc = sectionNum;
+    //         };
+    
+    //         // Adds one Instagram icon every time through the for loop up until the number specified in the data
+
+            
+
+    //         for(let i=0; i < globalData[sectionNum].apps[i]; i++) {
+    //             const randomNum1 = Math.floor(Math.random()*1300);
+    //             const randomNum2 = Math.floor(Math.random()*700);
+    
+    //             document.getElementById('icons').innerHTML += `<i id="${apps[i]}${i}_${sectionNum}" class="fab fa-${apps[i]}-square" style="left:${randomNum1}px; top:${randomNum2}px;"></i>`;
+    //         };
+    //     };
+    // };
+
     
 })();
